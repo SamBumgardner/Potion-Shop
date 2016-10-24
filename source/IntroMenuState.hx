08b769c925c2f5f58fc85d47d77f12bd6d3ab3c2
Sam Bumgardner <sambumgardner1@gmail.com>
2016-10-24 00:41:56 -0500

    Added simple buttons to the IntroMenuState
    This is a very basic demo to demonstrate the basic version of the Button class.

--- source/IntroMenuState.hx
+++ source/IntroMenuState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flash.system.System;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -11,6 +12,9 @@ class IntroMenuState extends FlxState
 {
 	override public function create():Void
 	{
+		add(new Button(720, 375, AssetPaths.PlaceholderButton__png, 500, 100, function(){}, function(){FlxG.switchState(new ShopState()); }, function(){}, function(){}));
+		add(new Button(720, 500, AssetPaths.PlaceholderButton__png, 500, 100, function(){}, function(){}, function(){}, function(){}));
+		add(new Button(720, 625, AssetPaths.PlaceholderButton__png, 500, 100, function(){}, function(){System.exit(0);}, function(){}, function(){}));
 		super.create();
 	}
 
