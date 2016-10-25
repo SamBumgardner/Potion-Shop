3bcd4710c97640a6121d9d848693cd845703635d
Sam Bumgardner <sambumgardner1@gmail.com>
2016-10-25 09:34:54 -0500

    Made states un-register all buttons before switching
    Previously, buttons were never getting removed from the FlxMouseEventManager
    object. This could probably cause a bunch of problems, including causing a crash
    if buttons in the two states overlap and the mouse is hovering over the overlap point
    when the state is switched. This happened because the mouse manager was
    trying to access a field of a button that no longer existed.
    
    This commit makes the states responsible for un-registering all buttons before
    switching. I also tried making the buttons responsible for un-registering themselves
    when killed, but it didn't solve the issue.

--- source/IntroMenuState.hx
+++ source/IntroMenuState.hx
@@ -4,6 +4,7 @@ import flash.system.System;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.input.mouse.FlxMouseEventManager;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
@@ -16,6 +17,12 @@ class IntroMenuState extends FlxState
 		add(new Button(720, 500, AssetPaths.PlaceholderButton__png, 500, 100, function(){}, function(){}, function(){}, function(){}));
 		add(new Button(720, 625, AssetPaths.PlaceholderButton__png, 500, 100, function(){}, function(){System.exit(0);}, function(){}, function(){}));
 		super.create();
+	}
+	
+	override public function switchTo(nextState:FlxState):Bool
+	{
+		FlxMouseEventManager.removeAll();
+		return super.switchTo(nextState);
 	}
 
 	override public function update(elapsed:Float):Void
--- source/ShopState.hx
+++ source/ShopState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.input.mouse.FlxMouseEventManager;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
@@ -11,7 +12,14 @@ class ShopState extends FlxState
 {
 	override public function create():Void
 	{
+		add(new Button(720, 350, AssetPaths.PlaceholderButton__png, 500, 100, function(){}, function(){FlxG.switchState(new IntroMenuState()); }, function(){}, function(){}));
 		super.create();
+	}
+	
+	override public function switchTo(nextState:FlxState):Bool
+	{
+		FlxMouseEventManager.removeAll();
+		return super.switchTo(nextState);
 	}
 
 	override public function update(elapsed:Float):Void
