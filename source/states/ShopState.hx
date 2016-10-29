709310c48b46cdb1a2ce7b85e146ca5caddee587
Sam Bumgardner <sambumgardner1@gmail.com>
2016-10-29 11:40:23 -0500

    Implemented CustomerCard initializing function to ShopState.
    The numbers involved in positioning the cards have largely been stored in
    variables, which should hopefully make the positioning of the cards easier
    to tweak in the future.

--- source/states/ShopState.hx
+++ source/states/ShopState.hx
@@ -2,6 +2,7 @@ package states;
 
 import buttons.Button;
 import buttons.staticData.BrewTab;
+import buttons.staticData.CustomerCard;
 import buttons.staticData.CustomerTab;
 import buttons.staticData.InventoryTab;
 import buttons.staticData.QuitGame;
@@ -17,6 +18,7 @@ import flixel.math.FlxMath;
 class ShopState extends AdvancedState
 {
 	private var sideTabs:FlxTypedGroup<Button>;
+	private var custCards:FlxTypedGroup<Button>;
 	
 	override public function create():Void
 	{
@@ -26,6 +28,7 @@ class ShopState extends AdvancedState
 		setUpBackground(AssetPaths.ShopBg__png);
 		
 		initSideTabs();
+		initCustCards();
 		
 		add(new Button(720, 800, QuitGame));
 	}
@@ -43,6 +46,30 @@ class ShopState extends AdvancedState
 		sideTabs.add(new Button(sideTabX, middleSideTabY + YInterval, InventoryTab));
 		
 		add(sideTabs);
+	}
+	
+	private function initCustCards():Void
+	{
+		custCards = new FlxTypedGroup<Button>();
+		
+		var numOfVisibleCards = 4;
+		
+		var XIntervalMod = 1.01;
+		var YIntervalMod = 1.01;
+		var topLeftX = FlxG.width - CustomerCard.frameWidth * (2.2 * XIntervalMod);
+		var topLeftY = FlxG.height - CustomerCard.frameHeight * (2.2 * YIntervalMod);
+		var XInterval = CustomerCard.frameWidth * XIntervalMod;
+		var YInterval = CustomerCard.frameHeight * YIntervalMod;
+		
+		for (row in 0...(cast numOfVisibleCards / 2))
+		{
+			for (col in 0...2)
+			{
+				custCards.add(new Button(topLeftX + col * XInterval, topLeftY + row * YInterval, CustomerCard));
+			}
+		}
+		
+		add(custCards);
 	}
 	
 	private function switchActiveTab(button:Button)
