fc0a4d2f67e7bafbe42681f8aa3a19b9d87d0c4b
Sam Bumgardner <sambumgardner1@gmail.com>
2016-11-07 01:14:47 -0600

    Used inheritance to trim Tab child classes.

--- source/buttons/staticData/InventoryTab.hx
+++ source/buttons/staticData/InventoryTab.hx
@@ -1,69 +1,21 @@
 package buttons.staticData;
 
-import flixel.FlxG;
-import flixel.system.FlxAssets.FlxGraphicAsset;
-import flixel.tweens.FlxEase;
-import flixel.tweens.FlxTween;
+import buttons.Tab;
 import utilities.ShopButtonGroup;
 
 /**
- * Data needed to instantiate an "open inventory state" Button.
- * This class's fields should usually be accessed from within the Button class. 
+ * Data needed to instantiate an "open inventory" tab.
  * 
  * @author Samuel Bumgardner
  */
-class InventoryTab
+class InventoryTab extends Tab
 {
-	public static var image:FlxGraphicAsset = AssetPaths.InventoryStateButton__png;
-	public static var frameWidth:Int = 150;
-	public static var frameHeight:Int = 250;
-	
-	private static var hoverXOffset:Int = 30;
-	public static var activeXOffset:Int = 28;
-	public static var activeYOffset:Int = 0;
-
-	public static function mouseUp(button:Button):Void
+	public function new(?X:Float = 0, ?Y:Float = 0, ?beginActive:Bool)
 	{
-		if (!button.isActive)
-		{
-			(cast FlxG.state).switchShopMode(button, Inventory);
-		}
-	}
-	
-	public static function mouseDown(button:Button):Void{}
-	
-	public static function mouseOver(button:Button):Void
-	{
-		if (button.currentTween != null)
-		{
-			button.currentTween.cancel();
-		}
-		FlxTween.tween(button, {x: button.anchorX - hoverXOffset}, .3, {ease: FlxEase.circOut});
-	}
-	
-	public static function mouseOut(button:Button):Void
-	{
-		if (button.currentTween != null)
-		{
-			button.currentTween.cancel();
-		}
-		FlxTween.tween(button, {x: button.anchorX}, .3, {ease: FlxEase.circOut});
-	}
-	
-	public static function activate(button:Button):Void
-	{
-		button.anchorX -= activeXOffset;
-		hoverXOffset -= activeXOffset;
-	}
-	
-	public static function deactivate(button:Button):Void
-	{
-		button.anchorX += activeXOffset;
-		hoverXOffset += activeXOffset;
-		if (button.currentTween != null)
-		{
-			button.currentTween.cancel();
-		}
-		FlxTween.tween(button, {x: button.anchorX}, .3, {ease: FlxEase.circOut});
+		image = AssetPaths.InventoryStateButton__png;
+		
+		buttonGroup = Inventory;
+		
+		super(X, Y, beginActive);
 	}
 }
