2543e90f8b52bc7cda08f83cdfe51a7443ac665b
Sam Bumgardner <sambumgardner1@gmail.com>
2016-11-06 22:09:15 -0600

    Reduced Button functionality & changed static function declarations.
    These changes have two main purposes: to create a bare-bones version of
    Button that can be used by buttons that have limited functionality by design and
    to set up the possibility of classes inheriting from Button, now that there is
    some non-static functionality.

--- source/buttons/Button.hx
+++ source/buttons/Button.hx
@@ -4,7 +4,6 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.input.mouse.FlxMouseEventManager;
 import flixel.system.FlxAssets.FlxGraphicAsset;
-import flixel.tweens.FlxTween;
 
 /**
  * Sets default animation and functions behavior
@@ -16,99 +15,17 @@ import flixel.tweens.FlxTween;
 class Button extends FlxSprite
 {
 	/**
-	 * Read-only: Function that will be called when the left mouse button is released while
-	 * over this button. Should only be used internally. 
-	 */
-	private var MUp:Button->Void;
-	
-	/**
-	 * Read-only: Function that will be called when the left mouse button is pressed while
-	 * over this button. Should only be used internally. 
-	 */
-	private var MDown:Button->Void;
-	
-	/**
-	 * Read-only: Function that will be called when the mouse enters this button's area.
-	 * Should only be used internally. 
-	 */
-	private var MOver:Button->Void;
-	
-	/**
-	 * Read-only: Function that will be called when the mouse leaves this button's area.
-	 * Should only be used internally. 
-	 */
-	private var MOut:Button->Void;
-	
-	/**
-	 * The original X position of this button.
-	 * Used by tweening/animating buttons so they have an unchanging point of reference.
-	 */
-	public var anchorX:Float;
-	
-	/**
-	 * The original Y position of this button.
-	 * Used by  tweening/animating buttons so they have an unchanging point of reference.
-	 */
-	public var anchorY:Float;
-	
-	public var currentTween:FlxTween;
-	
-	/**
-	 * Boolean flag that tracks whether this button is "active" or not.
-	 * Up to the button implementer how to use this, exactly.
-	 */
-	public var isActive:Bool = false;
-	
-	/**
-	 * Read-only: Function called when button is activated.
-	 * Should only be used internally. 
-	 */
-	private var MActivate:Button->Void;
-	
-	/**
-	 * Read-only: Function called when button is deactivated.
-	 * Should only be used internally. 
-	 */
-	private var MDeactivate:Button->Void;
-	
-	/**
 	 * @param	X             The initial X position of the button.
 	 * @param	Y             The initial Y position of the button.
-	 * @param	buttonClass   The class that contains the static variables and functions 
-	 *                        the button should use - e.g. Button(500, 450, Simple)
-	 * @param	beginActive   Applies adjusted positioning and any other necessary changes
-	 *       	              to the button so that it appears active upon instantiation.
 	 */
-	public function new(?X:Float = 0, ?Y:Float = 0, buttonClass, ?beginActive:Bool)
+	public function new(?X:Float = 0, ?Y:Float = 0)
 	{
-		anchorX = X;
-		anchorY = Y;
-		
-		if (beginActive)
-		{
-			X -= buttonClass.activeXOffset;
-			Y -= buttonClass.activeYOffset;
-		}
-		
 		super(X, Y);
 		
 		loadGraphic(buttonClass.image, true, buttonClass.frameWidth, buttonClass.frameHeight);
 		animation.add("Normal", [0], 1, false);
 		animation.add("Hover", [1], 1, false);
 		animation.add("Pressed", [2], 1, false);
-		
-		MUp   = buttonClass.mouseUp;
-		MDown = buttonClass.mouseDown;
-		MOver = buttonClass.mouseOver;
-		MOut  = buttonClass.mouseOut;
-		
-		MActivate   = buttonClass.activate;
-		MDeactivate = buttonClass.deactivate;
-		
-		if (beginActive)
-		{
-			Button.activate(this);
-		}
 		
 		Button.register(this);
 	}
@@ -120,8 +37,8 @@ class Button extends FlxSprite
 	 */
 	public static function register(button:Button):Void
 	{
-		FlxMouseEventManager.add(button, Button.MouseDown, Button.MouseUp, 
-		                         Button.MouseOver, Button.MouseOut);
+		FlxMouseEventManager.add(button, button.MouseDown, button.MouseUp, 
+		                         button.MouseOver, button.MouseOut);
 	}
 	
 	/**
@@ -154,34 +71,31 @@ class Button extends FlxSprite
 	 * Callback function registered to the FlxMouseEventManager.
 	 * @param	button	The instance of button that was acted upon by the mouse.
 	 */
-	public static function MouseUp(button:Button):Void
+	public function MouseUp(button:Button):Void
 	{
 		button.animation.play("Hover");
-		button.MUp(button);
 	}
 	
 	/**
 	 * Callback function registered to the FlxMouseEventManager.
 	 * @param	button	The instance of button that was acted upon by the mouse.
 	 */
-	public static function MouseDown(button:Button):Void
+	public function MouseDown(button:Button):Void
 	{
 		button.animation.play("Pressed");
-		button.MDown(button);
 	}
 	
 	/**
 	 * Callback function registered to the FlxMouseEventManager.
 	 * @param	button	The instance of button that was acted upon by the mouse.
 	 */
-	public static function MouseOver(button:Button):Void
+	public function MouseOver(button:Button):Void
 	{
 		button.animation.play("Hover");
-		button.MOver(button);
 		
 		if (FlxG.mouse.pressed)
 		{ 
-			Button.MouseDown(button);
+			button.MouseDown(button);
 		}
 	}
 	
@@ -189,37 +103,9 @@ class Button extends FlxSprite
 	 * Callback function registered to the FlxMouseEventManager.
 	 * @param	button	The instance of button that was acted upon by the mouse.
 	 */
-	public static function MouseOut(button:Button):Void
+	public function MouseOut(button:Button):Void
 	{
 		button.animation.play("Normal");
 		button.MOut(button);
-	}
-	
-	/**
-	 * Function that "activates" the button. 
-	 * The logic of what this does is pretty much all on the button function class.
-	 * @param	button	The instance of button that was activated.
-	 */
-	public static function activate(button:Button):Void
-	{
-		if (!button.isActive)
-		{
-			button.isActive = true;
-			button.MActivate(button);
-		}
-	}
-	
-	/**
-	 * Function that "deactivates" the button. 
-	 * The logic of what this does is pretty much all on the button function class.
-	 * @param	button	The instance of button that was deactivated.
-	 */
-	public static function deactivate(button:Button):Void
-	{
-		if (button.isActive)
-		{
-			button.isActive = false;
-			button.MDeactivate(button);
-		}
 	}
 }
