f22ca7abc002df874bc3d058ec6ced8c9fc6fd1c
Sam Bumgardner <sambumgardner1@gmail.com>
2016-11-20 16:06:44 -0600

    Added NO_TYPE entry to ButtonTypes enum.
    This should be used whenever I need to indicate the absence
    of type, like when representing that a button was not pressed on
    a particular frame.

--- source/utilities/ButtonEvent.hx
+++ source/utilities/ButtonEvent.hx
@@ -8,6 +8,7 @@ typedef ButtonEvent = Int;
 @:enum
 class EventData 
 {
+	public static var NO_EVENT = -1;
 	public static var OUT = 0;
 	public static var OVER = 1;
 	public static var DOWN = 2;
@@ -17,6 +18,7 @@ class EventData
 @:enum
 class ButtonTypes 
 {
+	public static var NO_TYPE(default, never) = -1;
 	public static var BUTTON(default, never) = 0;
 	public static var TAB(default, never) = 1;
 	public static var ING_HEX(default, never) = 2;
