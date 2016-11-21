e9db8dfcc1e6712df755c7aad9f598ab6aee7999
Sam Bumgardner <sambumgardner1@gmail.com>
2016-11-20 22:09:03 -0600

    Added NO_EVENT & NO_TYPE enum values.
    EventData and ButtonTypes felt like they should specify some
    invalid value that can be referenced when checking for events
    with unset data.
    
    This "invalid value" may be better off in some different place than
    these enums, though. If it were, I may be able to find some way
    to reference the number of entries in the enum for determining
    things like the # of different valid button types.

--- source/utilities/ButtonEvent.hx
+++ source/utilities/ButtonEvent.hx
@@ -8,19 +8,19 @@ typedef ButtonEvent = Int;
 @:enum
 class EventData 
 {
-	public static var NO_EVENT = -1;
-	public static var OUT = 0;
-	public static var OVER = 1;
-	public static var DOWN = 2;
-	public static var UP = 3;
+	public static var NO_EVENT(default, never) = -1;
+	public static var OUT(default, never)      = 0;
+	public static var OVER(default, never)     = 1;
+	public static var DOWN(default, never)     = 2;
+	public static var UP(default, never)       = 3;
 }
 
 @:enum
 class ButtonTypes 
 {
-	public static var NO_TYPE(default, never) = -1;
-	public static var BUTTON(default, never) = 0;
-	public static var TAB(default, never) = 1;
-	public static var ING_HEX(default, never) = 2;
+	public static var NO_TYPE(default, never)    = -1;
+	public static var BUTTON(default, never)     = 0;
+	public static var TAB(default, never)        = 1;
+	public static var ING_HEX(default, never)    = 2;
 	public static var SELECT_HEX(default, never) = 3;
 }
