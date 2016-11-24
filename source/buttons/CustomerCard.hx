366b08a37dbe02e6b58cfa52a4abe9ab73bed130
Sam Bumgardner <sambumgardner1@gmail.com>
2016-11-23 23:35:27 -0600

    Changed CustomerCard functionality.
    Previously, clicking on CustomerCard would open up a substate
    with options to either quit the game or close the substate.
    
    Now, it functions much like the Cauldron class, used in BrewState.
    When activated it is in a permanent "hovered" state until deactivation.
    It fires off notifications when it recieves a mouseUp event, etc.

--- source/buttons/CustomerCard.hx
+++ source/buttons/CustomerCard.hx
@@ -1,30 +1,72 @@
 package buttons;
 
+import buttonTemplates.ActiveButton;
 import buttonTemplates.Button;
 import flixel.FlxG;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 import states.CustomerDetails;
+import utilities.ButtonEvent;
+import utilities.Subject;
+
+using utilities.EventExtender;
 
 /**
- * Data needed to instantiate a "open options" Button.
- * This class's fields should only be accessed from within the Button class. 
+ * Container-Button that holds a customer's data and
+ * responds to different gameplay situations.
  * 
  * @author Samuel Bumgardner
  */
-class CustomerCard extends Button
+class CustomerCard extends ActiveButton implements Observer
 {
+	public var sub:Subject = new Subject(0, ButtonTypes.CUSTOMER);
+	
 	public function new(?X:Float = 0, ?Y:Float = 0)
 	{
 		image = AssetPaths.CustomerCard__png;
-		bWidth = 800;
-		bHeight = 400;
+		bWidth = 600;
+		bHeight = 225;
 		
 		super(X, Y);
+	override public function mouseOver(button:Button):Void
+	{
+		if (!isActive)
+		{
+			super.mouseOver(button);
+		}
+	}
+	
+	override public function mouseOut(button:Button):Void
+	{
+		if (!isActive)
+		{
+			super.mouseOut(button);
+		}
+	}
+	
+	override public function mouseDown(button:Button):Void
+	{
+		if (!isActive)
+		{
+			super.mouseDown(button);
+		}
 	}
 	
 	override public function mouseUp(button:Button):Void
 	{
-		super.mouseUp(button);
-		(cast FlxG.state).activateSubstate(this, CustomerDetails);
+		if (!isActive)
+		{
+			super.mouseUp(button);
+			sub.notify(EventData.UP);
+		}
+	}
+	
+	override private function mActivate():Void 
+	{
+		animation.play("Hover");
+	}
+	
+	override private function mDeactivate():Void 
+	{
+		animation.play("Normal");
 	}
 }
