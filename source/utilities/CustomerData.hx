4181e6721e1d89fa2c1f026e229258f924ecb549
Sam Bumgardner <sambumgardner1@gmail.com>
2016-12-05 00:32:21 -0600

    Created CustomerData class.
    Simlar to CauldronData and PotionData, this class
    exists to hold (and in some cases, generate) customer-
    related information. It also has some other functionality,
    such as checking if another potion satisfies the customer's
    request.

---
+++ source/utilities/CustomerData.hx
@@ -0,0 +1,54 @@
+package utilities;
+
+/**
+ * A class for holding details about a customer.
+ * @author Samuel Bumgardner
+ */
+class CustomerData
+{
+	public var name:String;
+	public var graphicIndex:Int;
+	public var desiredPotion:PotionData;
+	public var pay:Int;
+	
+	public function new(Name:String, GraphicIndex:Int, potionColVals:Array<Int>, Pay:Int)
+	{
+		name = Name;
+		graphicIndex = GraphicIndex;
+		desiredPotion = new PotionData(potionColVals);
+		pay = Pay;
+	}
+	
+	public function getRequestDescription():String
+	{
+		var colorConverter:ColorConverter = new ColorConverter();
+		
+		var wouldLikeA:String = "I would like a ";
+		var color:String = colorConverter.intToColorStr[desiredPotion.colorByIndex]
+		                   .toLowerCase() + " ";
+		var potion:String = "potion with...\n";
+		
+		return wouldLikeA + color + potion + desiredPotion.description;
+	}
+	
+	public function checkPotion(potion:PotionData):Bool
+	{
+		var isAcceptable:Bool = true;
+		for (i in 0...desiredPotion.activeEffects.length)
+		{
+			for (j in 0...desiredPotion.activeEffects[i].length)
+			{
+				if (potion.activeEffects[i][j] < desiredPotion.activeEffects[i][j])
+				{
+					isAcceptable = false;
+					break;
+				}
+			}
+			if (!isAcceptable)
+			{
+				break;
+			}
+		}
+		return isAcceptable;
+	}
+}
